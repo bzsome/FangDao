88b2d5f8b706568bae8218f3abdeb771978e9b0c
bzsome <35402969+bzsome@users.noreply.github.com>
2018-05-13 10:42:59 +0800

    编译输出apk

--- app/src/main/java/com/bzchao/chao/fangdao/Until/MyLog.java
+++ app/src/main/java/com/bzchao/chao/fangdao/Until/MyLog.java
@@ -16,17 +16,15 @@ import java.util.Date;
 /**
  * Android开发调试日志工具类[支持保存到SD卡]<br>
  * <br>
- *
+ * <p>
  * 需要一些权限: <br>
  * <uses-permission android:name="android.permission.WRITE_EXTERNAL_STORAGE" /> <br>
  * <uses-permission android:name="android.permission.MOUNT_UNMOUNT_FILESYSTEMS" /><br>
  *
  * @author PMTOAM
- *
  */
 @SuppressLint("SimpleDateFormat")
-public class MyLog
-{
+public class MyLog {
 
     public static final String CACHE_DIR_NAME = ".aTestDir";
 
@@ -34,99 +32,85 @@ public class MyLog
     public static boolean isSaveDebugInfo = true;// 是否保存调试日志
     public static boolean isSaveCrashInfo = true;// 是否保存报错日志
 
-    public static void v(final String tag, final String msg)
-    {
-        if (isDebugModel)
-        {
+    public static void v(final String tag, final String msg) {
+        if (isDebugModel) {
             Log.v(tag, "--> " + msg);
         }
     }
 
-    public static void d(final String tag, final String msg)
-    {
-        if (isDebugModel)
-        {
-           MyLog.e(tag, "--> " + msg);
+    public static void d(final String tag, final String msg) {
+        if (isDebugModel) {
+            Log.d(tag, "--> " + msg);
         }
     }
 
-    public static void i(final String tag, final String msg)
-    {
-        if (isDebugModel)
-        {
+    public static void i(final String tag, final String msg) {
+        if (isDebugModel) {
             Log.i(tag, "--> " + msg);
         }
     }
 
-    public static void w(final String tag, final String msg)
-    {
-        if (isDebugModel)
-        {
+    public static void w(final String tag, final String msg) {
+        if (isDebugModel) {
             Log.w(tag, "--> " + msg);
         }
     }
 
     /**
      * 调试日志，便于开发跟踪。
+     *
      * @param tag
      * @param msg
      */
-    public static void e(final String tag, final String msg)
-    {
-        if (isDebugModel)
-        {
+    public static void e(final String tag, final String msg) {
+        if (isDebugModel) {
             Log.e(tag, "--> " + msg);
         }
 
-        if (isSaveDebugInfo)
-        {
-            new Thread()
-            {
-                public void run()
-                {
+        if (isSaveDebugInfo) {
+            new Thread() {
+                public void run() {
                     write(time() + tag + " --> " + msg + "\n");
-                };
+                }
+
+                ;
             }.start();
         }
     }
 
     /**
      * try catch 时使用，上线产品可上传反馈。
+     *
      * @param tag
      * @param tr
      */
-    public static void e(final String tag, final Throwable tr)
-    {
-        if (isSaveCrashInfo)
-        {
-            new Thread()
-            {
-                public void run()
-                {
+    public static void e(final String tag, final Throwable tr) {
+        if (isSaveCrashInfo) {
+            new Thread() {
+                public void run() {
                     write(time() + tag + " [CRASH] --> "
                             + getStackTraceString(tr) + "\n");
-                };
+                }
+
+                ;
             }.start();
         }
     }
 
     /**
      * 获取捕捉到的异常的字符串
+     *
      * @param tr
      * @return
      */
-    public static String getStackTraceString(Throwable tr)
-    {
-        if (tr == null)
-        {
+    public static String getStackTraceString(Throwable tr) {
+        if (tr == null) {
             return "";
         }
 
         Throwable t = tr;
-        while (t != null)
-        {
-            if (t instanceof UnknownHostException)
-            {
+        while (t != null) {
+            if (t instanceof UnknownHostException) {
                 return "";
             }
             t = t.getCause();
@@ -140,10 +124,10 @@ public class MyLog
 
     /**
      * 标识每条日志产生的时间
+     *
      * @return
      */
-    private static String time()
-    {
+    private static String time() {
         return "["
                 + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date(
                 System.currentTimeMillis())) + "] ";
@@ -151,38 +135,35 @@ public class MyLog
 
     /**
      * 以年月日作为日志文件名称
+     *
      * @return
      */
-    private static String date()
-    {
+    private static String date() {
         return new SimpleDateFormat("yyyy-MM-dd").format(new Date(System
                 .currentTimeMillis()));
     }
 
     /**
      * 保存到日志文件
+     *
      * @param content
      */
-    public static synchronized void write(String content)
-    {
-        try
-        {
+    public static synchronized void write(String content) {
+        try {
             FileWriter writer = new FileWriter(getFile(), true);
             writer.write(content);
             writer.close();
-        }
-        catch (IOException e)
-        {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     /**
      * 获取日志文件路径
+     *
      * @return
      */
-    public static String getFile()
-    {
+    public static String getFile() {
         File sdDir = null;
 
         if (Environment.getExternalStorageState().equals(
