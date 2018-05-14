41ddfb01bbe92d6c2a75291a79aee2ee454f9b73
bzsome <35402969+bzsome@users.noreply.github.com>
2018-05-14 13:59:44 +0800

    新增：双进程守护，防止被杀

---
+++ app/src/main/java/com/bzchao/chao/fangdao/keepalive/utils/Contants.java
@@ -0,0 +1,12 @@
+package com.bzchao.chao.fangdao.keepalive.utils;
+
+/**全局常量
+ *
+ * Created by jianddongguo on 2017/7/8.
+ */
+
+public class Contants {
+    public static final boolean DEBUG = true;
+
+    public static final String PACKAGE_NAME = "com.bzchao.com.fangdao";
+}
